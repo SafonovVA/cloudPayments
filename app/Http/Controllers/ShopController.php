4df73d7241be96cd93a9fb07f058d15afc95d07c
Vladimir Safonov <safonov.open@gmail.com>
2021-04-23 17:18:26 +0600

    added integrity with national bank

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\ExchangeRate;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ShopController extends Controller
 {
@@ -12,6 +14,17 @@ class ShopController extends Controller
     {
         $products = Product::all();
         $categories = Category::all();
+
+        // For Cron
+        /*$response = Http::get('https://nationalbank.kz/rss/rates_all.xml');
+        $XMLResponse = new \SimpleXMLElement($response);
+        foreach($XMLResponse->channel->item as $item) {
+            ExchangeRate::updateOrCreate(
+                ['title' => $item->title],
+                ['value' => $item->description]
+            );
+        }*/
+        //End cron
 
         return view('index', compact('products', 'categories'));
     }
