646b9ae823caa937c319093d5b8d0f276b1bf9e9
Vladimir Safonov <safonov.open@gmail.com>
2021-04-25 22:27:30 +0600

    change product orderBy on main page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use App\Models\Category;
 use App\Models\Product;
 
 Route::get('/', fn() => view('index', [
-    'products' => Product::all(),
-    'categories' => Category::orderBy('category_id')->orderBy('created_at')->get()])
+    'products' => Product::orderBy('category_id')->orderBy('created_at')->get(),
+    'categories' => Category::all()])
 );
